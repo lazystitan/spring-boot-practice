24b7334fc44443695f0d1e8ab2bf2dbbf29fb93e
Riton <ritonelion@outlook.com>
2019-03-29 18:48:28 +0800

    成功启动

---
+++ src/main/java/com/ritonelion/bootopen/BootopenApplication.java
@@ -0,0 +1,15 @@
+package com.ritonelion.bootopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootopenApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BootopenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ritonelion/bootopen/controller/HelloSpringBootController.java
@@ -0,0 +1,14 @@
+package com.ritonelion.bootopen.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBootController
+{
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello spring boot";
+    }
+}
